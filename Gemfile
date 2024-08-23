53eefaa2ba3a770719b80ddf1d36ce849e5dfa03
Yasfi <yasfi.mail@gmail.com>
2024-08-24 06:55:01 +0700

    setup

---
+++ Gemfile
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+source "https://rubygems.org"
+
+# Specify your gem's dependencies in watermelon_db_sync.gemspec
+gemspec
+
+gem "rake", "~> 13.0"
---
+++ lib/watermelon_db_sync.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "watermelon_db_sync/version"
+
+module WatermelonDbSync
+  class Error < StandardError; end
+  # Your code goes here...
+end
